31e5b5e1b5a086913e794a69b03d4be723809a9e
om prakash <omprakash_choudhary@condenast.com>
2021-10-13 16:41:21 +0530

    Add Heroku deployment

--- main.py
+++ main.py
@@ -14,26 +14,33 @@ app.config.from_mapping(
 Bootstrap(app)
 
 @app.route('/', methods=['GET', 'POST'])
+@app.route('/index.html', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
 @app.route('/maps', methods=['GET', 'POST'])
+@app.route('/maps.html', methods=['GET', 'POST'])
 def maps():
     return render_template('maps.html')
 
 @app.route('/explore', methods=['GET', 'POST'])
+@app.route('/explore.html', methods=['GET', 'POST'])
 def explore():
     return render_template('explore.html')
 
 @app.route('/donate', methods=['GET', 'POST'])
+@app.route('/donate.html', methods=['GET', 'POST'])
 def donate():
     return render_template('donate.html')
 
 @app.route('/details', methods=['GET', 'POST'])
+@app.route('/details.html', methods=['GET', 'POST'])
 def details():
     return render_template('campaign-details.html')
 
 @app.route('/campaigns', methods=['GET', 'POST'])
+@app.route('/campaigns.html', methods=['GET', 'POST'])
+@app.route('/campaign-details.html', methods=['GET', 'POST'])
 def campaigns():
     return render_template('campaigns.html')
 
